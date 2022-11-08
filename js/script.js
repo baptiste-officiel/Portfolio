40a4038abaed02e70ac1bfc3f11851a3ffa26087
Baptiste <baptiste.lejeune@hotmail.com>
2022-11-08 11:45:23 +0100

    Dépôt Portfolio

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// --------------------------------- menu burger ----------------------------- 
+let menu = document.getElementById('menu');
+let burger = document.getElementById('burger')
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('show');
+    console.log(window)
+});
+
+menu.addEventListener('click', function(){
+    menu.classList.remove('show');
+})
+
+
+// ------------------------------------ compétences ----------------------------- 
+const competence = document.querySelectorAll('#list-competences .competence')
+let competences = document.getElementById('list-competences');
+
+// competences.addEventListener('click', function(){
+//     // competence.classList.add('.animation');
+//     // competence.forEach(comp => {
+//     //     comp.classList.add('animation')
+//     //     console.log(comp.innerHTML);
+//     // });
+// })
+
+let options = {
+    root: null,
+    rootMargin: "-5% 0px",
+    treshold: 0.2
+};
+
+function handleIntersect(entries){
+    console.log(entries)
+
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            // console.log(entry.target.innerHTML)
+            entry.target.classList.add('animation')
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+competence.forEach(comp => {
+   observer.observe(comp);
+});
---
+++ js/supabase.js
@@ -0,0 +1,36 @@
+const { createClient } = supabase;
+
+// Create a single supabase client for interacting with your database 
+supabase = createClient('https://htmuqkpvnbzotfgdsgnj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0bXVxa3B2bmJ6b3RmZ2RzZ25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4Mzg1MTEsImV4cCI6MTk4MzQxNDUxMX0.vdi7NsnnMc4DC11N8vi29NIGU1Yxz1Y1ZXtJDg5g4wc')
+
+console.log(supabase)
+
+const form = document.querySelector('#contact-form');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formInputs = form.querySelectorAll('input, textarea');
+
+    let submission = {};
+
+    formInputs.forEach(element => {
+        const { value, name } = element;
+        if (value) {
+            submission[name] = value;
+            console.log(element.value)
+        }
+    })
+
+    
+    const { error, data } = await supabase.from('contact-portfolio').insert([submission]);
+    // console.log({ error, data });
+
+    if (error) {
+        alert('Il y a une erreur dans le formulaire, essayez à nouveau s\'il vous plaît');
+        
+    } else {
+        alert('Le formulaire a bien été envoyé !');
+    }
+
+    formInputs.forEach(element => element.value = '');
+})
